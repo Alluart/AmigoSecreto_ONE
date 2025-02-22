77aca5a5b252f6679bac8bed0c84d77643ff8631
thiago <thiagoagostinho6@gmail.com>
2025-02-22 00:12:26 -0300

    verificação do campo

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = []
+let amigos = [];
 
+//função verifica se o campo esta vazio e adiciona no array amigos
 function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
 
+    if (nome == ''){
+        alert('Por favor, insira um nome.')
+    } else {
+        amigos.push(nome);
+    }
+    limparCampo();
 }
 
-function sortearAmigo() {
-
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
 }
+
+console.log(amigos)
