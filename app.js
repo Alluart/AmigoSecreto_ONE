16ce8503f54dfe2b6632f410ce91560b6127b8dd
thiago <thiagoagostinho6@gmail.com>
2025-02-22 22:34:02 -0300

    função adicionar amigos

--- app.js
+++ app.js
@@ -6,11 +6,29 @@ function adicionarAmigo() {
     let nome = document.querySelector('input').value;
 
     if (nome == ''){
-        alert('Por favor, insira um nome.')
+        alert('Por favor, insira um nome.');
     } else {
         amigos.push(nome);
+        adicionarNome();
     }
     limparCampo();
+}
+
+//função mostra a lista na tela 
+function adicionarNome(){
+    let listaNomes = document.getElementById('listaAmigos');
+    listaNomes.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++){
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        listaNomes.appendChild(item);
+    }
+}
+
+//funçao sorteia um nomes da lista
+function sortearAmigo(){
+    
 }
 
 function limparCampo() {
